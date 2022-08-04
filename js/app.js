5555c63e05a7e97a64dcec5bfcc8edc2fdc6cc0d
rasalgh0ul <arkantos983@gmail.com>
2022-08-04 19:56:53 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 function cambiarTitulo(){
-    console.log("Desde la funcion cambiar titulo")
+    console.log("Desde la funcion cambiar titulo");
+    //buscar del html el elemento que me interesa
+    let titulo = document.querySelector("h1");
+    // let titulo = document.getElementsByTagName("h1");
+    console.log(titulo)
+    titulo.innerHTML = "Titulo Nuevo"
 }
 
